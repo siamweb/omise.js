5842ab6355227d09992083b4a1f830d5d3e2810c
Tommy Omise <tommy@omise.co>
2015-04-30 17:29:56 +0700

    Handle timeout when trying to connect to provider (setTimeout to 30 secs)

--- src/omise.js
+++ src/omise.js
@@ -13,13 +13,26 @@
   Omise.easyXDM = easyXDM.noConflict('Omise');
   Omise.easyXDM.DomHelper.requiresJSON(Omise._config.assetUrl + "/json2.js");
 
-  Omise._createRpc = function(){
+  Omise._createRpc = function(handler){
     if (Omise._rpc) {
       return Omise._rpc;
     } else {
+      var tm = setTimeout(function() {
+        Omise._rpc.destroy();
+        Omise._rpc = null;
+        
+        if(handler){
+          handler(0, { code: "rpc_error", 
+                       message: "unable to connect to provider after 30 seconds" })
+        }
+      },30000);
+      
       Omise._rpc = new Omise.easyXDM.Rpc({
         remote: Omise._config.vaultUrl + "/provider",
-        swf: Omise._config.assetUrl + "/easyxdm.swf"
+        swf: Omise._config.assetUrl + "/easyxdm.swf",
+        onReady: function (){
+          clearTimeout(tm) 
+        }
       }, {remote: {createToken: {}}});
       return Omise._rpc;
     }
@@ -35,9 +48,12 @@
   Omise.createToken = function(as, attributes, handler) {
     var data = {};
     data[as] = attributes;
-    Omise._createRpc().createToken(Omise.publicKey, data, function(response) {
+    
+    Omise._createRpc(handler).createToken(Omise.publicKey, data, function(response) {
       handler(response.status, response.data);
-    }, function(){ /* noop */ });
+    }, function(e){
+      handler(e.data.status, e.data.data);
+    });
   };
 
   // exports
